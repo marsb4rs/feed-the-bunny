8fccf4ccafe4cfa3d2e79a92050ffa5b6829a844
marsb4rs <87779733+marsb4rs@users.noreply.github.com>
2021-08-31 13:44:36 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,6 +11,7 @@ let engine;
 let world;
 var rope,fruit,ground;
 var fruit_con;
+var fruit_con_2;
 
 var bg_img;
 var food;
@@ -18,35 +19,52 @@ var rabbit;
 
 var button;
 var bunny;
+var blink,eat,sad;
 
 function preload()
 {
   bg_img = loadImage('background.png');
   food = loadImage('melon.png');
-  rabbit = loadImage('Rabbit-01.png');
+  rabbit = loadImage('Rabbit-01.png');;
+  blink = loadAnimation("blink_1.png","blink_2.png","blink_3.png");
+  eat = loadAnimation("eat_0.png" , "eat_1.png","eat_2.png","eat_3.png","eat_4.png");
+  sad = loadAnimation("sad_1.png","sad_2.png","sad_3.png");
+  
+  blink.playing = true;
+  eat.playing = true;
+  sad.playing = true;
+  sad.looping= false;
+  eat.looping = false; 
 }
 
-function setup() 
-{
+function setup() {
   createCanvas(500,700);
   frameRate(80);
 
   engine = Engine.create();
   world = engine.world;
-
-  //btn 1
+  
   button = createImg('cut_btn.png');
-  button.position(200,30);
+  button.position(220,30);
   button.size(50,50);
   button.mouseClicked(drop);
-
   
-  rope = new Rope(8,{x:220,y:30});
+  rope = new Rope(7,{x:245,y:30});
   ground = new Ground(200,690,600,20);
-  bunny = createSprite(200,620,100,100);
-  bunny.addImage(rabbit);
+
+  blink.frameDelay = 20;
+  eat.frameDelay = 20;
+  sad.frameDelay = 20;
+
+  bunny = createSprite(230,620,100,100);
   bunny.scale = 0.2;
 
+  bunny.addAnimation('blinking',blink);
+
+  bunny.addAnimation('eating',eat);
+  bunny.addAnimation('crying',sad);
+  bunny.changeAnimation('blinking');
+  
   fruit = Bodies.circle(300,300,20);
   Matter.Composite.add(rope.body,fruit);
 
@@ -54,28 +72,34 @@ function setup()
 
   rectMode(CENTER);
   ellipseMode(RADIUS);
-  textSize(50)
+  imageMode(CENTER);
   
 }
 
 function draw() 
 {
   background(51);
-  image(bg_img,0,0,displayWidth+80,displayHeight);
+  image(bg_img,width/2,height/2,490,690);
 
-  push();
-  imageMode(CENTER);
   if(fruit!=null){
     image(food,fruit.position.x,fruit.position.y,70,70);
   }
-  pop();
 
   rope.show();
-
   Engine.update(engine);
   ground.show();
-  drawSprites();
+
+  if(collide(fruit,bunny)==true)
+  {
+    bunny.changeAnimation('eating');
+  }
    
+  if(collide(fruit,ground.body)==true )
+  {
+     bunny.changeAnimation('crying');
+   }
+
+   drawSprites();
 }
 
 function drop()
@@ -85,7 +109,19 @@ function drop()
   fruit_con = null; 
 }
 
-
-
-
-
+function collide(body,sprite)
+{
+  if(body!=null)
+        {
+         var d = dist(body.position.x,body.position.y,sprite.position.x,sprite.position.y);
+          if(d<=80)
+            {
+              World.remove(engine.world,fruit);
+               fruit = null;
+               return true; 
+            }
+            else{
+              return false;
+            }
+         }
+}
